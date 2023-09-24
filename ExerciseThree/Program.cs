4fc2ca52add8b3a239daa6de032118b6783c0201
gulnazgentle-kazan <gulnazvp@mail.ru>
2023-09-25 01:09:07 +0300

    did exercise three

---
+++ ExerciseThree/Program.cs
@@ -0,0 +1,40 @@
+﻿/*Задача №3. Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+3 -> Среда 
+5 -> Пятница
+*/
+
+Console.WriteLine("Введите любое число от 1 до 7 ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 1 || number > 7)
+{
+    Console.WriteLine("Введите число в заданном диапазоне от 1 до 7");
+}
+if (number == 1)
+{
+    Console.WriteLine("Понедельник");
+}
+if (number == 2)
+{
+    Console.WriteLine("Вторник");
+}
+if (number == 3)
+{
+    Console.WriteLine("Среда");
+}
+if (number == 4)
+{
+    Console.WriteLine("Четверг");
+}
+if (number == 5)
+{
+    Console.WriteLine("Пятница");
+}
+if (number == 6)
+{
+    Console.WriteLine("Суббота");
+}
+if (number == 7)
+{
+    Console.WriteLine("Воскресенье");
+}
