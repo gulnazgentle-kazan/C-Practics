be048f0fa3f1ea87d3f8febc965c3d3d2dd8b428
gulnazgentle-kazan <gulnazvp@mail.ru>
2023-09-25 22:14:52 +0300

    did exercise 7

---
+++ ExerciseSeven/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Задача №7. Напишите программу, которая принимает на вход трёхзначное число 
+и на выходе показывает последнюю цифру этого числа.
+	456 -> 6
+	782 -> 2
+	918 -> 8
+*/
+
+Console.WriteLine("Напишите любое трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 100 || number > 999)
+{
+	Console.WriteLine("Напишите любое трехзначное число");
+}
+else
+{
+	int numberTwo = number % 10;
+	Console.WriteLine(numberTwo);
+}
