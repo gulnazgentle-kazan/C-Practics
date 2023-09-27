7a0727a568f13c07ad73830a58b265c5d8cb9645
gulnazgentle-kazan <gulnazvp@mail.ru>
2023-09-27 20:19:06 +0300

    did excercise 11

---
+++ Excercise11/Program.cs
@@ -0,0 +1,13 @@
+﻿/*Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа.
+
+456 -> 46
+782 -> 72
+918 -> 98
+*/
+Random rnd = new Random();
+int number = rnd.Next(99, 1000);
+Console.WriteLine(number);
+int del1 = (number / 100);
+int del2 = number % 10;
+int result = del1 * 10 + del2;
+Console.WriteLine(result);
