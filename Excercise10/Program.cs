53228d1b15f09d8e24613544cf5de2697f82d7bc
gulnazgentle-kazan <gulnazvp@mail.ru>
2023-09-27 21:36:01 +0300

    did

---
+++ Excercise10/Program.cs
@@ -0,0 +1,26 @@
+﻿/* 
+Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает 
+вторую цифру этого числа.
+
+
+
+456 -> 5
+782 -> 8 
+918 -> 1
+
+
+*/
+
+Console.WriteLine("Введите любое трехзначное число ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int result = number / 10 % 10;
+
+if (number < 100 || number > 1000)
+{
+    Console.WriteLine("Введите трехзначное число ");
+}
+else
+{
+    Console.WriteLine(result);
+}
