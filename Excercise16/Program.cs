892c4d6aeb979cca60b71f43925a300cf615a8a8
gulnazgentle-kazan <gulnazvp@mail.ru>
2023-09-27 21:25:19 +0300

    did

---
+++ Excercise16/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Напишите программу, которая принимает на вход два числа и проверяет, 
+является ли одно число квадратом другого.
+
+5, 25  ->  да
+-4, 16  ->  да
+25, 5  ->  да
+8,9  ->  нет
+*/
+
+Console.WriteLine("Введите любое число ");
+int numberOne = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите любое число ");
+int numberTwo = Convert.ToInt32(Console.ReadLine());
+
+if (numberOne * numberOne == numberTwo || numberTwo * numberTwo == numberOne)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
