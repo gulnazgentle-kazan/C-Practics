29f6b7f071062f704ba3a69f5bb4b25e2b7c32b4
gulnazgentle-kazan <gulnazvp@mail.ru>
2023-09-28 22:22:49 +0300

    did

---
+++ Excercise28/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+4 -> 24
+5 -> 120*/
+
+Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+int result = 1;
+int Sum(int A)
+{
+    while (count < A)
+    {
+        count++;
+        result = result * count;
+
+    }
+    return result;
+}
+
+Console.WriteLine(Sum(a));
