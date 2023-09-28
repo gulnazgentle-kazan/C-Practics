2b58209371d6553cc7f73634ab4d6a44d3a17238
gulnazgentle-kazan <gulnazvp@mail.ru>
2023-09-28 22:18:28 +0300

    did

---
+++ Excercise26/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+456 -> 3
+78 -> 2
+89126 -> 5*/
+
+
+Console.WriteLine("Введите любое число");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+
+int Count(int arg1)
+{
+    while (number != 0)
+    {
+        count++;
+        number = number / 10;
+    }
+    return count;
+}
+
+Console.WriteLine(Count(number));
