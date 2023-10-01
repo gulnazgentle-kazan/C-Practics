019f0364aee9e2f7cd03087ce13febac16f93e33
gulnazgentle-kazan <gulnazvp@mail.ru>
2023-10-01 19:51:20 +0300

    did

---
+++ Excercise29/Program.cs
@@ -0,0 +1,31 @@
+﻿/*Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. 
+Данные вводятся с консоли пользователем*/
+
+
+int[] array = new int[8];
+
+void FillArray(int[] collection)
+{
+    int index = 0;
+    int length = collection.Length;
+    while (index < length)
+    {
+        Console.WriteLine("Введите любое число ");
+        collection[index] = Convert.ToInt32(Console.ReadLine());
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int index = 0;
+    int size = col.Length;
+    while (index < size)
+    {
+        Console.WriteLine(col[index]);
+        index++;
+    }
+}
+
+FillArray(array);
+PrintArray(array);
